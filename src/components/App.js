62cb99ffd8dc2d11f389c8f3b1711885a50027d4
Rahul Singh <rahulsinghrajpoot9997@gmail.com>
2026-01-08 08:12:44 +0530

    Import sumCalculator component in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 
 import React from "react";
 import './../styles/App.css';
+import sumCalculator from "Sumcalculator"
 
 const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
+    <sumCalculator />
     </div>
   )
 }
